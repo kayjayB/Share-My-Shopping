ebacd64c279c7ad07fc1ad052425ab1ba31b84dc
Jared <jaredping@yahoo.com>
2018-04-18 09:07:16 +0200

    Base config + router

---
+++ mainRoutes.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var express = require('express');
+var mainRouter = express.Router();
+
+mainRouter.get('/', function (req, res) {
+	res.send('Hello World, I\'m Node.js');
+});
+
+mainRouter.get('/about', function(req, res){
+	res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+module.exports = mainRouter;
