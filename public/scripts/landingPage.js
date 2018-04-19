90c1880e988d62e96907a49e204210e1f138149f
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-04-19 18:21:30 +0200

    Render shopping list item name onto the screen

--- public/scripts/landingPage.js
+++ public/scripts/landingPage.js
@@ -1,10 +1,14 @@
 let shoppingList = [];
 
 function addItem() {
+
     let item = document.getElementById("ShoppingListItem").value;
 
     shoppingList.push(item);
 
     // Clear input text field once the item has been saved to the array
     document.getElementById("ShoppingListItem").value = "";
+
+    document.getElementById("shoppingList").innerHTML = shoppingList[0];
+
 }
