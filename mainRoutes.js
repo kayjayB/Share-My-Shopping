b183fe1eb59769fa0994e78b7c0f25250aa0901a
Matthew van Rooyen <706692@students.wits.ac.za>
2018-04-19 10:00:12 +0200

    Landing Page Addition

--- mainRoutes.js
+++ mainRoutes.js
@@ -3,11 +3,12 @@ var express = require('express');
 var mainRouter = express.Router();
 
 mainRouter.get('/', function (req, res) {
-	res.send('Hello World, I\'m Node.js');
+	res.sendFile(path.join(__dirname, 'views', 'landingPage.html'));
 });
 
 mainRouter.get('/about', function(req, res){
 	res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
+
 
 module.exports = mainRouter;
