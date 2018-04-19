dfdbcb43ac9c905d9e1db352060861857c995443
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-04-19 17:12:57 +0200

    Tests for adding an item to shopping list

--- test/base.js
+++ test/base.js
@@ -1,8 +1,29 @@
 "use strict";
 let test = require("tape");
 
-test("Hello World: hello should greet the world", function(t){
+test("Hello World: hello should greet the world", function(t) {
     let hello = "world";
     t.equal(hello, "world");
+    t.end();
+});
+
+test("An empty shopping list has a length of 0", function(t) {
+    let item = "First Item";
+    let shoppingList = [];
+
+    let sizeArray = shoppingList.length;
+
+    t.equal(sizeArray, 0);
+    t.end();
+});
+
+test("Adding a shopping list item to an empty list increases the size of the list to 1", function(t) {
+    let item = "First Item";
+    let shoppingList = [];
+
+    shoppingList.push(item);
+    let sizeArray = shoppingList.length;
+
+    t.equal(sizeArray, 1);
     t.end();
 });
