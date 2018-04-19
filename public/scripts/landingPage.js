4be7587f41d83e66d9204392a52166fb4236e1e0
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-04-19 19:03:42 +0200

    Created a card to display the shopping list item. Added styling so that the card moves when the mouse hovers over it.
    TODO: Find more efficient way to add a child card so that a shopping list item is not repeated

--- public/scripts/landingPage.js
+++ public/scripts/landingPage.js
@@ -9,6 +9,29 @@ function addItem() {
     // Clear input text field once the item has been saved to the array
     document.getElementById("ShoppingListItem").value = "";
 
-    document.getElementById("shoppingList").innerHTML = shoppingList[0];
+    // Get the element that will contain the cards
+    let container = document.getElementById('list-container')
 
+    // Remove all cards before re-adding new cards. 
+    // This ensures that there are no duplicate cards
+    // TODO: More efficient way to do this
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Create the cards and append the card to the parent element
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    cardDiv.id = "list-entry";
+
+    let headerElement = document.createElement("h4");
+    headerElement.id = "shoppingList";
+
+    let headerText = document.createTextNode(shoppingList[0]);
+
+    headerElement.appendChild(headerText);
+
+    cardDiv.appendChild(headerElement);
+
+    container.appendChild(cardDiv);
 }
