a56551f034acfa70738d6af803bae970776f420d
Lara Timm <l_timm@hotmail.com>
2018-04-21 11:06:42 +0200

    Disable button if no text input

--- public/scripts/landingPage.js
+++ public/scripts/landingPage.js
@@ -1,16 +1,27 @@
 let shoppingList = [];
 let shoppingListCategory = [];
 
+function toggleButton() {
+
+    // Check that an item has been added
+    let item = document.getElementById("ShoppingListItem").value;
+
+    if(item.length == 0) {
+        document.getElementById("SubmitButton").disabled = true;
+    }
+    else {
+        document.getElementById("SubmitButton").disabled = false;
+    }
+}
+
+
 function addItem() {
 
     let item = document.getElementById("ShoppingListItem").value;
     let category = document.getElementById("ShoppingListCategory").value;
     
-    // Check that an item has been added
-    if(item.length == 0) {
-        window.alert("Please enter an item to add to your list");
-        return;
-    }
+
+ 
     // Check that the category is not empty, if so assign a default value
     if(category.length == 0) {
         category = " "
@@ -24,6 +35,9 @@ function addItem() {
     // Clear input text field once the item has been saved to the array
     document.getElementById("ShoppingListItem").value = "";
     document.getElementById("ShoppingListCategory").value = "";
+
+    // Disable the button again for no input
+    document.getElementById("SubmitButton").disabled = true;
 
     // Get the element that will contain the cards
     let container = document.getElementById('list-container')
